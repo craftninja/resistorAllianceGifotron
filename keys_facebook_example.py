229a7fc88b1b6ba82e2e74494c3ca3d1b67b758e
Emily Platzer <emilyplatzer@gmail.com>
2017-09-21 13:32:09 -0600

    Skeleton for facebook integration

---
+++ keys_facebook_example.py
@@ -0,0 +1,1 @@
+fbook_token = 'your consumer key'
---
+++ testFiles/testFacebook.py
@@ -0,0 +1,17 @@
+import facebook
+from keys_facebook import *
+
+print("awaiting token")
+# fbook = facebook.GraphAPI(access_token=fbook_token, version="2.10")
+#
+# fbook.put_photo(image=open('images/test.jpg', 'rb'),
+#                 message='testing Gifotron')
+#
+# try:
+#     fbook.put_photo(
+#         image=open('images/test.jpg', 'rb'),
+#         message='testing Gifotron'
+#     )
+#     print("uploaded")
+# except Exception as e:
+#     print(e)
